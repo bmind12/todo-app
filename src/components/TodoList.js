173a2443c63e5a6b959fa5b04db2e27433c075a8
Sergey Ryabov <sergey.ryabov@actum.cz>
2018-02-21 20:43:34 +0100

    Cleaning

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react';
+import propTypes from 'prop-types';
 import { List } from 'antd';
 import TodoItem from './TodoItem';
 import type { Task } from '../types';
@@ -38,5 +39,16 @@ const TodoList = (props: Props) => {
         />
     )
 }
+    
+TodoList.propTypes = {
+    data: propTypes.array,
+    editTodo: propTypes.func.isRequired,
+    removeTodo: propTypes.func.isRequired,
+    toggleTodo: propTypes.func.isRequired
+};
+
+TodoList.defaultProps = {
+    data: []
+};
 
 export default TodoList;
